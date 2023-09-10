90517e6752e6c4efdffed8c58e1ab0f352dd2e6a
Jigyasu-vatsa <62921979+Jigyasu-vatsa@users.noreply.github.com>
2023-09-10 19:51:25 +0530

    Add files via upload

---
+++ Dr_B_lgn_pg.js
@@ -0,0 +1,46 @@
+function cnf() {
+  let chk = prompt(usrnm.value , pwd.value);
+  if (chk == null) {
+    window.location.reload();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+function flaga(){ 
+if(usrnm.value=="" || pwd.value==""){ 
+f() 
+document.getElementById("usrnm").style.border="5px solid red" 
+document.getElementById("pwd").style.border="5px solid red" 
+bt.value="Insert Value First" 
+} 
+else{ 
+document.getElementById("usrnm").style.border="5px solid green" 
+document.getElementById("pwd").style.border="5px solid green" 
+bt.value="Now Press Submit" 
+bt.style.left="44%"; 
+} 
+} 
+flag=1 
+function f(){ 
+if(flag==1){ 
+bt.style.left="30%"
+flag=2
+}
+else if(flag==2){ 
+bt.style.left="57%" 
+flag=1 
+} 
+} 
+
