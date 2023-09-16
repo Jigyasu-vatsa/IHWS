7939f9cc93920106afa6187116c2950c7939d1d2
Jigyasu-vatsa <62921979+Jigyasu-vatsa@users.noreply.github.com>
2023-09-16 22:48:44 +0530

    Update Dr_B_lgn_pg.js

--- Dr_B_lgn_pg.js
+++ Dr_B_lgn_pg.js
@@ -1,9 +1,14 @@
 function cnf() {
   let chk = prompt(usrnm.value , pwd.value);
-  if (chk == null) {
+}
+
+
+
+
+/* if (chk == null) {
     window.location.reload();
   }
-}
+*/
 
 
 
@@ -29,17 +34,17 @@ else{
 document.getElementById("usrnm").style.border="5px solid green" 
 document.getElementById("pwd").style.border="5px solid green" 
 bt.value="Now Press Submit" 
-bt.style.left="44%"; 
+bt.style.left="46.7%"; 
 } 
 } 
 flag=1 
 function f(){ 
 if(flag==1){ 
-bt.style.left="30%"
+bt.style.left="32%"
 flag=2
 }
 else if(flag==2){ 
-bt.style.left="57%" 
+bt.style.left="60%" 
 flag=1 
 } 
 } 
